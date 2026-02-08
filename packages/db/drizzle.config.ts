db142bded04ddf676dbe164341e3a6b628542840
Julian Sigmund <pixelmund@gmail.com>
2026-02-08 22:29:03 +0100

    feat: add console log for DATABASE_URL in drizzle.config.ts

--- packages/db/drizzle.config.ts
+++ packages/db/drizzle.config.ts
@@ -1,10 +1,14 @@
 import dotenv from "dotenv";
 import { defineConfig } from "drizzle-kit";
 
+console.log(process.env.DATABASE_URL);
+
 dotenv.config({
 	path: "../../apps/server/.env",
 	debug: true,
 });
+
+console.log(process.env.DATABASE_URL);
 
 export default defineConfig({
 	schema: "./src/schema",
